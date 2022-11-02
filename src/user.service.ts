3070f4f188c9651d33c5be541ba30571795ae60a
Mbogning <juniormbogning@gmail.com>
2022-11-02 10:43:26 +0100

    nest crud

---
+++ src/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, Repository } from "typeorm";
+import { UserEntity } from "./user";
+
+@Injectable()
+export class UserService{
+    constructor(
+        @InjectRepository(UserEntity)
+        private userRepository: Repository<UserEntity>
+    ){}
+
+    findAll(): Promise<UserEntity[]>{
+        return this.userRepository.find();
+    }
+
+    // findOne(id: number): Promise<UserEntity>{
+    //     return this.userRepository.findOne(id);
+    // }
+
+    create(user: UserEntity): Promise<UserEntity>{
+        return this.userRepository.save(user);
+    }
+
+    async update(id: number, user: UserEntity) {
+        await this.userRepository.update(id, user);
+    }
+
+    async remove(id: number): Promise<void>{
+        await this.userRepository.delete(id);
+    }
+
+
+}
+
